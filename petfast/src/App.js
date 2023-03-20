6fc62d2cbc0b37a1306b4530f8116d54556ad174
RrafaelR <augusto.ac.teixeira@gmail.com>
2023-03-20 10:48:14 -0300

    adicionado pagina de registro do usuário

--- petfast/src/App.js
+++ petfast/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Pages/Login/index.js';
 import Home from './components/Pages/Home/index.js'
 import Carrinho from './components/Pages/Carrinho/index.js'
+import UserRegistration from './components/Pages/UserRegistration/UserRegistration.js'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path="/carrinho" element={<Carrinho/>}/>
+        <Route path="/registrar" element={<UserRegistration/>}/>
       </Routes>
     </Router>
   );
